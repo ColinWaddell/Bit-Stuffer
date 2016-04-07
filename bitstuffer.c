d91387cf277a2dc5f18a182bcecc9b0244a9c193
Colin <mrcolin@gmail.com>
2016-04-07 09:51:27 +0100

    pedentary

--- bitstuffer.c
+++ bitstuffer.c
@@ -5,17 +5,19 @@
 void print_b(uint8_t *in, uint32_t N);
 uint32_t bit_stuffer(uint8_t *in, uint8_t *out, uint32_t N);
 
-// Number of 1's which need a 0 appended
+/* Number of 1's which need a 0 appended */
 #define BIT_STUFF_ONES 5
 
-// Print array 'in' with length 'N' as binary stream
+/* Print array 'in' with length 'N' as binary stream */
 void print_b(uint8_t *in, uint32_t N) {
   uint8_t b=7;
   uint16_t i=0;
   while(N--){
     putchar('0' + ((in[i]>>(b)) & 1));
     b = (b-1)&0x07;
-    if(b==7) i++;
+    if(b==7){
+      i++;
+    }
   }
 }
 
@@ -41,15 +43,15 @@ void print_b(uint8_t *in, uint32_t N) {
  * The +1 is for good luck.
  */
 uint32_t bit_stuffer(uint8_t *in,
-                     uint8_t *out,
-                     uint32_t N)
+    uint8_t *out,
+    uint32_t N)
 {
-  uint16_t i      = 0;        // input byte counter
-  uint16_t o      = 0xFFFF;   // output byte counter
-  int8_t   b      = 7;        // input bit counter
-  uint16_t out_b  = 7;        // bit position of output
-  uint8_t  one_n  = 0;        // number of consequtive ones
-  uint8_t  bit    = 0;        // current bit
+  uint16_t i      = 0;        /* input byte counter         */
+  uint16_t o      = 0xFFFF;   /* output byte counter        */
+  int8_t   b      = 7;        /* input bit counter          */
+  uint16_t out_b  = 7;        /* bit position of output     */
+  uint8_t  one_n  = 0;        /* number of consequtive ones */
+  uint8_t  bit    = 0;        /* current bit                */
   uint32_t out_N = 0;
 
   /* o is initialised with 0xFFFF to force
@@ -63,48 +65,52 @@ uint32_t bit_stuffer(uint8_t *in,
    */
   out[0] = 0;
 
-  // Input byte loop
+  /* Input byte loop */
   while(N--){
-      out_N++;
-      if(out_b==7){
-        o = (o+1)&0xFFFF;
-        out[o] = 0;
-      }
-      first = 0;
-      bit = in[i]&(1<<b); // anything > 0 is a bit
+    out_N++;
+    if(out_b==7){
+      o = (o+1)&0xFFFF;
+      out[o] = 0;
+    }
 
-      if (bit){
-        // Bit is a 1
-        out[o] |= (1<<out_b);
-        one_n++;
-        if(one_n==BIT_STUFF_ONES){
-          // Update output counters to bit-stuff
-          out_N++;
-          out_b = (out_b-1)&0x07;
-          if(out_b==7){
-            o = (o+1)&0xFFFF;
-            out[o] = 0;
-          }
+    bit = in[i]&(1<<b); /* anything > 0 is a bit */
 
-          one_n = 0; // Reset counter
+    if (bit){
+      /* Bit is a 1 */
+      out[o] |= (1<<out_b);
+      one_n++;
+      if(one_n==BIT_STUFF_ONES){
+        /* Update output counters to bit-stuff */
+        out_N++;
+        out_b = (out_b-1)&0x07;
+        if(out_b==7){
+          o = (o+1)&0xFFFF;
+          out[o] = 0;
         }
-      }
-      else{
-        // Bit is a 0
-        // Reset counter
+        /* Reset counter */
         one_n = 0;
       }
-      // Update output counters
-      out_b = (out_b-1)&0x07;
-      b = (b-1)&0x07;
-      if(b==7) i++;
+    }
+    else{
+      /* Bit is a 0    */
+      /* Reset counter */
+      one_n = 0;
+    }
+    /* Update output counters */
+    out_b = (out_b-1)&0x07;
+    b = (b-1)&0x07;
+    if(b==7){
+      /* move to the next input
+       * input array element */
+      i++;
+    }
   }
 
   return out_N;
 }
 
 int main(void){
-  // Sample data
+  /* Sample data */
   uint8_t in[] =
   { 0b00111110,
     0b00101010,
@@ -115,22 +121,22 @@ int main(void){
     0b10101111,
     0b10000000 };
 
-  // See above for calculation for length of out.
+  /* See above for calculation for length of out. */
   uint8_t out[sizeof(in)+(sizeof(in)/5)+1];
-  // Length of output after bit-stuffing
-  uint16_t ;
+  /* Length of output after bit-stuffing          */
+  uint16_t out_n;
 
-  // Print input data
+  /* Print input data */
   printf("input data:\n");
   print_b(in, sizeof(in)*8);
 
-  // Stuff them bits
-   = bit_stuffer(in, out, sizeof(in)*8);
+  /* Stuff them bits */
+  out_n = bit_stuffer(in, out, sizeof(in)*8);
 
-  // Print output data
+  /* Print output data */
   printf("\noutput data:\n");
-  print_b(out, );
-  printf("\nNumber of output bits: %d\n", );
+  print_b(out, out_n);
+  printf("\nNumber of output bits: %d\n", out_n);
 
   return 0;
 }
